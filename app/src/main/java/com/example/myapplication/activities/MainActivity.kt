a74205ecb1c19fde6f44937f6c7a543182348cab
Roberto HErnadnez <00162317@uca.edu.sv>
2019-04-05 14:00:00 -0600

    Cree unas variables

---
+++ app/src/main/java/com/example/myapplication/activities/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import com.example.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var movieAdapter: MovieADapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+    private var movieList:ArrayList<Movie> = ArrayList()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
