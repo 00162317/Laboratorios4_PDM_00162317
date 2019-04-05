34d78f7ba62bb7c0a34da6d48d645e784924571c
Roberto HErnadnez <00162317@uca.edu.sv>
2019-04-05 14:14:26 -0600

    El linear layout para ver la orientacion

--- app/src/main/java/com/example/myapplication/activities/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/MainActivity.kt
@@ -2,14 +2,16 @@ package com.example.myapplication.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.example.myapplication.R
+import com.example.myapplication.pojos.Movie
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var movieAdapter: MovieADapter
-    private lateinit var viewManager: Recycler
-    View.LayoutManager
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     private var movieList:ArrayList<Movie> = ArrayList()
 
@@ -18,6 +20,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initRecyclerView()
 
+    }
+
+    private fun initRecyclerView() {
+        viewManager = LinearLayoutManager(this)
+
+        movieAdapter=MovieAdapter(movieList,{movieItem:Movie->movieItemClicked(movieItem)})
+
+        movie_list_rv.apply { setHasFixedSize(true)
+        layoutManager=viewManager
+        adapter=movieAdapter
+        }
     }
 }
