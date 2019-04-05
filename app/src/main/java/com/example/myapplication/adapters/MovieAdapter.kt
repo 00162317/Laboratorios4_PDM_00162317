f187ddf2eaa90e71025d06925d5d12381e510133
Roberto HErnadnez <00162317@uca.edu.sv>
2019-04-05 14:29:45 -0600

    Se creo la clase View Holder

--- app/src/main/java/com/example/myapplication/adapters/MovieAdapter.kt
+++ app/src/main/java/com/example/myapplication/adapters/MovieAdapter.kt
@@ -1,12 +1,19 @@
 package com.example.myapplication.adapters
 
+import android.support.v7.view.menu.ActionMenuItemView
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.example.myapplication.R
 import com.example.myapplication.pojos.Movie
+import kotlinx.android.synthetic.main.cardview_movie.view.*
 
 class MovieAdapter(var movies: List<Movie>,val clickListener:(Movie)->Unit): RecyclerView.Adapter<MovieAdapter.ViewHolder>(){
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ViewHolder {
+       val view = LayoutInflater.from(parent.context).inflate(R.layout.cardview_movie,parent,false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -17,8 +24,16 @@ class MovieAdapter(var movies: List<Movie>,val clickListener:(Movie)->Unit): Rec
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    class ViewHolder {
+    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+        fun bind(item:Movie,clickListener: (Movie) -> Unit)= with(itemView){
+            Glide.with(itemView.context).load(item.Poster).placeholder(R.drawable.ic_launcher_background).into(movie_image_cv)
 
+            movie_title_cv.text=item.Title
+            movie_plot_cv.text=item.Plot
+            movie_rate_cv.text=item.imdbRating
+            movie_runtime_cv.text=item.Runtime
+            this.setOnClickListener{clickListener(item)}
+        }
     }
 
 }
